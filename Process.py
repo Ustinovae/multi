20a911a97c81c7e412668d9fccd361b682542c2a
Ustinovae <ustinov.alescha2015@yandex.ru>
2021-12-01 00:05:02 +0500

    modified Process.py

---
+++ Process.py
@@ -0,0 +1,48 @@
+import concurrent.futures
+import math
+import multiprocessing
+import time
+
+PRIMES = [
+    112272535095293,
+    112582705942171,
+    112272535095293,
+    115280095190773,
+    115797848077099,
+    1099726899285419]
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+def plus_100(b):
+    while (b > 0):
+        b -= 1
+
+
+def process():
+    with multiprocessing.Pool(20) as p:
+        t1 = time.time()
+        print(p.map(plus_100, (50005000, )), time.time() - t1)
+
+
+def main():
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
+            print('%d is prime: %s' % (number, prime))
+
+
+if __name__ == '__main__':
+    main()
