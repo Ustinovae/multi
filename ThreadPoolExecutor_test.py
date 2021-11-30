faf4e3da6d8ba2cdba9c5f8eee059332e79f1926
Ustinovae <ustinov.alescha2015@yandex.ru>
2021-11-30 23:48:42 +0500

    Add checking non-working links in one multiple thread(5, 10, 100)

---
+++ ThreadPoolExecutor_test.py
@@ -0,0 +1,21 @@
+import concurrent.futures
+import urllib.request
+
+URLS = open('res.txt', encoding='utf8').read().split('\n')
+
+
+def load_url(url, timeout):
+    with urllib.request.urlopen(url, timeout=timeout) as conn:
+        return conn.read()
+
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    future_to_url = {executor.submit(load_url, url, 5): url for url in URLS}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            data = future.result()
+        except Exception as exc:
+            print('%r generated an exception: %s' % (url, exc))
+        else:
+            print('%r page is %d bytes' % (url, len(data)))
